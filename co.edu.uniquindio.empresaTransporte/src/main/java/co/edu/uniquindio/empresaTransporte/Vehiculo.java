242444ba09fcab2e914404a0b319dadd9ef5b29f
jharpin <154296932+jharpin@users.noreply.github.com>
2025-02-10 21:20:38 -0500

    clase vehiculo 1

---
+++ co.edu.uniquindio.empresaTransporte/src/main/java/co/edu/uniquindio/empresaTransporte/Vehiculo.java
@@ -0,0 +1,55 @@
+package co.edu.uniquindio.empresaTransporte;
+
+public class Vehiculo {
+    public String marca;
+    public String modelo;
+    public String placa;
+    public String color;
+    public int numeroChasis;
+
+    public Vehiculo(String marca, String modelo, String placa, String color, int numeroChasis) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.placa = placa;
+        this.color = color;
+        this.numeroChasis = numeroChasis;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+    public void setMarca(String marca) {
+        this.marca = marca;
+
+    }
+    public String getModelo() {
+        return modelo;
+    }
+    public void setModelo(String modelo){
+        this.modelo=modelo;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa){
+        this.placa = placa;
+    }
+    public String getCorlor() {
+        return color;
+    }
+    public void setCorlor(String corlor){
+        this.color = corlor;
+    }
+    public int getNumeroChasis() {
+        return numeroChasis;
+    }
+    public void setNumeroChasis(int numeroChasis) {
+        this.numeroChasis = numeroChasis;
+    }
+
+    public String toString() {
+        return "El vehiculo tiene las caracteristicas de: "+marca + " " + modelo + " " + placa + " " + color + " " + numeroChasis;
+    }
+}
